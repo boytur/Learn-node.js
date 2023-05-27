15fb965a716740ff0d44867eeac41115dc217f03
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2023-05-27 22:28:22 +0700

    Add template
    Add template and connect path to index.html for static webpage

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3001 ;
+const express = require("express");
+const app = express();
+const port = 3001;
 
+//Connect to HTML
+//First re quir path
+const path = require("path");
 
-//เอาไว้จัดการ req 
+//ใช้ path เพื่อดึงหน้า index.html
+app.use(express.static(path.join(__dirname, "/public")));
+
+//เอาไว้จัดการ req
 //เขาเข้ามาหน้า "/" จะส่งอะไรให้เขาไป
-
-app.get('/', (req, res) =>{
-    //จะส่งอะไร
-    res.send('Halo Welcum...')
-})
+app.get("/", (req, res) => {
+  //จะส่งอะไร
+  res.send("Halo Welcum...");
+});
 
 //รอฟังที่พอร์ต 3000 อยากให้เขาทำอะไร ตอนนี้พอร์ตยังเปิดไหม
-app.listen(port, () =>{
-    console.log("Listening on port %d" , port);
-})
+app.listen(port, () => {
+  console.log("Listening on port %d", port);
+});
+
 
