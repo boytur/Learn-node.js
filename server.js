121738ec60cb764eeda3e24b4e646440739be4ba
Piyawat Wongyat <104257779+boytur@users.noreply.github.com>
2023-05-28 16:25:45 +0700

    add setting json

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT;
 
 //Connect to HTML
 //First re quir path
@@ -13,12 +13,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 //เขาเข้ามาหน้า "/" จะส่งอะไรให้เขาไป
 app.get("/", (req, res) => {
   //จะส่งอะไร
-  res.send("Halo Welcum...");
+  res.render("index",{username:'pramsa'});
 });
 
 //รอฟังที่พอร์ต 3000 อยากให้เขาทำอะไร ตอนนี้พอร์ตยังเปิดไหม
-app.listen(port, () => {
-  console.log("Listening on port %d", port);
+app.listen(PORT, () => {
+  console.log("Listening on port %d",PORT);
 });
 
 
+app.set("views","./src/views")
+app.set("views engine","ejs");
+
+//มาทําความรู้จักกับ Cluster ว่ามันช่วยเว็บแอปของเราได้ อย่างไร ?
